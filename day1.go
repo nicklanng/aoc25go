b7c0d973614794c77d93262d60ec9ee40a502942
nicklanng <rinse@rinseworld.com>
2025-12-01 21:59:01 +0000

    Initial commit

---
+++ day1.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("input/day1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	commands := strings.Split(string(input), "\n")
+
+	count := puzzle1(commands)
+	fmt.Println("Puzzle 1: ", count)
+
+	count = puzzle2(commands)
+	fmt.Println("Puzzle 2: ", count)
+}
+
+func puzzle1(commands []string) int {
+	count := 0
+	pos := 50
+
+	for _, command := range commands {
+		dir, stepsStr := command[0], command[1:]
+		steps, _ := strconv.Atoi(stepsStr)
+
+		switch dir {
+		case 'L':
+			pos -= steps
+		case 'R':
+			pos += steps
+		}
+
+		for {
+			if pos < 0 {
+				pos += 100
+			} else {
+				break
+			}
+		}
+
+		for {
+			if pos > 99 {
+				pos -= 100
+			} else {
+				break
+			}
+		}
+
+		if pos == 0 {
+			count++
+		}
+	}
+
+	return count
+}
+
+func puzzle2(commands []string) int {
+	count := 0
+	pos := 50
+
+	for _, command := range commands {
+		dir, stepsStr := command[0], command[1:]
+		steps, _ := strconv.Atoi(stepsStr)
+
+		switch dir {
+		case 'L':
+			for range steps {
+				pos -= 1
+				if pos == 0 {
+					count++
+				}
+				if pos == -1 {
+					pos = 99
+				}
+			}
+		case 'R':
+			for range steps {
+				pos += 1
+				if pos == 100 {
+					pos = 0
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
---
+++ day1_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestPuzzle1(t *testing.T) {
+	t.Run("Left 50 steps", func(t *testing.T) {
+		input := "L50"
+		commands := strings.Split(input, "\n")
+		count := puzzle1(commands)
+		if count != 1 {
+			t.Errorf("Expected 1, got %d", count)
+		}
+	})
+
+	t.Run("Right 50 steps", func(t *testing.T) {
+		input := "R50"
+		commands := strings.Split(input, "\n")
+		count := puzzle1(commands)
+		if count != 1 {
+			t.Errorf("Expected 1, got %d", count)
+		}
+	})
+
+	t.Run("Left 50 steps, then right 50 steps", func(t *testing.T) {
+		input := "L50\nR50"
+		commands := strings.Split(input, "\n")
+		count := puzzle1(commands)
+		if count != 1 {
+			t.Errorf("Expected 1, got %d", count)
+		}
+	})
+
+	t.Run("Right 49 steps", func(t *testing.T) {
+		input := "R49"
+		commands := strings.Split(input, "\n")
+		count := puzzle1(commands)
+		if count != 0 {
+			t.Errorf("Expected 0, got %d", count)
+		}
+	})
+
+	t.Run("Left 51 steps, Right 1 step", func(t *testing.T) {
+		input := "L51\nR1"
+		commands := strings.Split(input, "\n")
+		count := puzzle1(commands)
+		if count != 1 {
+			t.Errorf("Expected 1, got %d", count)
+		}
+	})
+
+	t.Run("Left 51 steps, Right 1 step", func(t *testing.T) {
+		input := "L51\nR1"
+		commands := strings.Split(input, "\n")
+		count := puzzle1(commands)
+		if count != 1 {
+			t.Errorf("Expected 1, got %d", count)
+		}
+	})
+
+	t.Run("Left 250 steps", func(t *testing.T) {
+		input := "L250"
+		commands := strings.Split(input, "\n")
+		count := puzzle1(commands)
+		if count != 1 {
+			t.Errorf("Expected 1, got %d", count)
+		}
+	})
+
+	t.Run("Right 250 steps", func(t *testing.T) {
+		input := "R250"
+		commands := strings.Split(input, "\n")
+		count := puzzle1(commands)
+		if count != 1 {
+			t.Errorf("Expected 1, got %d", count)
+		}
+	})
+}
+
+func TestPuzzle2(t *testing.T) {
+	t.Run("Left 50 steps", func(t *testing.T) {
+		input := "L50"
+		commands := strings.Split(input, "\n")
+		count := puzzle2(commands)
+		if count != 1 {
+			t.Errorf("Expected 1, got %d", count)
+		}
+	})
+
+	t.Run("Left 150 steps", func(t *testing.T) {
+		input := "L150"
+		commands := strings.Split(input, "\n")
+		count := puzzle2(commands)
+		if count != 2 {
+			t.Errorf("Expected 2, got %d", count)
+		}
+	})
+
+	t.Run("Left 1000 steps", func(t *testing.T) {
+		input := "L1000"
+		commands := strings.Split(input, "\n")
+		count := puzzle2(commands)
+		if count != 10 {
+			t.Errorf("Expected 10, got %d", count)
+		}
+	})
+
+	t.Run("Right 1000 steps", func(t *testing.T) {
+		input := "R1000"
+		commands := strings.Split(input, "\n")
+		count := puzzle2(commands)
+		if count != 10 {
+			t.Errorf("Expected 10, got %d", count)
+		}
+	})
+
+	t.Run("Left 1000 steps, Right 1000 steps", func(t *testing.T) {
+		input := "L1000\nR1000"
+		commands := strings.Split(input, "\n")
+		count := puzzle2(commands)
+		if count != 20 {
+			t.Errorf("Expected 20, got %d", count)
+		}
+	})
+}
