a082ebb3beafa323edf24936b7fa2f2761e1ca74
nicklanng <rinse@rinseworld.com>
2025-12-02 12:56:34 +0000

    Day2

---
+++ cmd/day02/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	Min int
+	Max int
+}
+
+func main() {
+	input, err := os.ReadFile("input/day2")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ranges := strings.Split(string(input), ",")
+
+	count := puzzle1(ranges)
+	fmt.Println("Puzzle 1: ", count)
+
+	count = puzzle2(ranges)
+	fmt.Println("Puzzle 2: ", count)
+}
+
+func puzzle1(rangesStr []string) int {
+	sum := 0
+
+	ranges := make([]Range, len(rangesStr))
+	for i, r := range rangesStr {
+		ranges[i] = parseRange(r)
+	}
+	for _, r := range ranges {
+		for i := r.Min; i <= r.Max; i++ {
+			if isSequenceRepeatedOnce(i) {
+				sum += i
+			}
+		}
+	}
+
+	return sum
+}
+
+func puzzle2(rangesStr []string) int {
+	sum := 0
+
+	ranges := make([]Range, len(rangesStr))
+	for i, r := range rangesStr {
+		ranges[i] = parseRange(r)
+	}
+	for _, r := range ranges {
+		for i := r.Min; i <= r.Max; i++ {
+			if isSequnceRepeatedAtLeastOnce(i) {
+				sum += i
+			}
+		}
+	}
+
+	return sum
+}
+
+// parseRange parses a range string into a Range struct
+func parseRange(r string) Range {
+	parts := strings.Split(r, "-")
+	min, _ := strconv.Atoi(parts[0])
+	max, _ := strconv.Atoi(parts[1])
+
+	// defensive, not sure if needed
+	if min > max {
+		return Range{Min: max, Max: min}
+	}
+
+	return Range{Min: min, Max: max}
+}
+
+// isSequenceRepeatedOnce checks if a number is a repeated sequence once
+func isSequenceRepeatedOnce(r int) bool {
+	// parse int to string
+	str := fmt.Sprintf("%d", r)
+
+	// split the string into two halves and check if they are equal
+	mid := len(str) / 2
+	return str[:mid] == str[mid:]
+}
+
+// isSequnceRepeatedAtLeastOnce checks if a number is a repeated sequence at least once
+func isSequnceRepeatedAtLeastOnce(r int) bool {
+	// parse int to string
+	str := fmt.Sprintf("%d", r)
+
+	// factor the length of the string
+	factors := factor(len(str))
+
+	// drop the first factor, coz 1 is just the whole string
+	factors = factors[1:]
+
+	// check if any factor is a repeated sequence
+	for _, f := range factors {
+		chunks := splitStringIntoChunks(str, f)
+		if allEqual(chunks) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// factor returns all factors of a number
+func factor(n int) []int {
+	factors := []int{}
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			factors = append(factors, i)
+		}
+	}
+	return factors
+}
+
+// splitStringIntoChunks splits a string into n chunks
+func splitStringIntoChunks(s string, n int) []string {
+	chunks := make([]string, n)
+	for i := range n {
+		chunks[i] = s[i*len(s)/n : (i+1)*len(s)/n]
+	}
+	return chunks
+}
+
+// allEqual checks if all elements in a slice are equal
+func allEqual[T comparable](s []T) bool {
+	first := s[0]
+	for _, v := range s {
+		if v != first {
+			return false
+		}
+	}
+	return true
+}
---
+++ cmd/day02/main_test.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseRange(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected Range
+	}{
+		{"1-10", Range{1, 10}},
+		{"10-1", Range{1, 10}},
+		{"234-345", Range{234, 345}},
+	}
+
+	for _, test := range tests {
+		if parseRange(test.input) != test.expected {
+			t.Errorf("Expected %v, got %v", test.expected, parseRange(test.input))
+		}
+	}
+}
+
+func TestIsSequenceRepeatedOnce(t *testing.T) {
+	tests := []struct {
+		input    int
+		expected bool
+	}{
+		// Invalid IDs (should return true)
+		{11, true},
+		{22, true},
+		{99, true},
+		{1010, true},
+		{1188511885, true},
+		{222222, true},
+		{446446, true},
+		{38593859, true},
+		// Valid IDs (should return false)
+		{1, false},
+		{12, false},
+		{95, false},
+		{100, false},
+		{998, false},
+		{1011, false},
+		{1012, false},
+		{1188511880, false},
+		{1188511881, false},
+		{222220, false},
+		{222221, false},
+		{1698522, false},
+		{1698523, false},
+		{1698528, false},
+		{446443, false},
+		{446444, false},
+		{446445, false},
+		{38593856, false},
+		{38593857, false},
+		{38593858, false},
+	}
+
+	for _, test := range tests {
+		if isSequenceRepeatedOnce(test.input) != test.expected {
+			t.Errorf("Expected %v, got %v", test.expected, isSequenceRepeatedOnce(test.input))
+		}
+	}
+}
+
+func TestIsSequnceRepeatedAtLeastOnce(t *testing.T) {
+	tests := []struct {
+		input    int
+		expected bool
+	}{
+		// Invalid IDs (should return true)
+		{11, true},
+		{22, true},
+		{99, true},
+		{111, true},
+		{999, true},
+		{1010, true},
+		{1188511885, true},
+		{222222, true},
+		{446446, true},
+		{38593859, true},
+		{565656, true},
+		{824824824, true},
+		{2121212121, true},
+		// Valid IDs (should return false)
+		{1, false},
+		{12, false},
+		{95, false},
+		{100, false},
+		{998, false},
+		{1011, false},
+		{1012, false},
+		{1188511880, false},
+		{1188511881, false},
+		{222220, false},
+		{222221, false},
+		{1698522, false},
+		{1698523, false},
+		{1698528, false},
+		{446443, false},
+		{446444, false},
+		{446445, false},
+		{565653, false},
+		{565654, false},
+		{565655, false},
+		{824824821, false},
+		{824824822, false},
+		{824824823, false},
+		{2121212118, false},
+		{2121212119, false},
+		{2121212120, false},
+	}
+
+	for _, test := range tests {
+		if isSequnceRepeatedAtLeastOnce(test.input) != test.expected {
+			t.Errorf("Expected %v for %d, got %v", test.expected, test.input, isSequnceRepeatedAtLeastOnce(test.input))
+		}
+	}
+}
+
+func TestFactor(t *testing.T) {
+	tests := []struct {
+		input    int
+		expected []int
+	}{
+		{1, []int{1}},
+		{2, []int{1, 2}},
+		{3, []int{1, 3}},
+		{4, []int{1, 2, 4}},
+	}
+
+	for _, test := range tests {
+		if !reflect.DeepEqual(factor(test.input), test.expected) {
+			t.Errorf("Expected %v, got %v", test.expected, factor(test.input))
+		}
+	}
+}
